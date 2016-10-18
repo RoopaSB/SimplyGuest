9a176c7d0ae48c9353a53d744332cc1af9d6fbeb
RoopaSB <roopa.sb@outlook.com>
2016-10-18 21:45:20 +0530

    Third Update

--- app/src/main/java/com/example/roopa/simplyguest/HomePage.java
+++ app/src/main/java/com/example/roopa/simplyguest/HomePage.java
@@ -1,12 +1,32 @@
 package com.example.roopa.simplyguest;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.Spinner;
 
-public class HomePage extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+import com.example.roopa.simplyguest.SpinnerNavItem;
+import com.example.roopa.simplyguest.MenuNavigationAdapter;
+
+public class HomePage extends Activity implements ActionBar.OnNavigationListener{
+
+    // Action Bar
+    private ActionBar actionBar;
+
+    //Menu navigation Spinner data
+
+    private ArrayList<SpinnerNavItem> navSpinner;
+    private MenuNavigationAdapter menuAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +37,36 @@ public class HomePage extends AppCompatActivity {
         ImageAdapter adapter = new ImageAdapter(this);
         viewPager.setAdapter(adapter);
 
-}
-    public void onClickLogin(View v){
+        actionBar = getActionBar();
+        // Hide the action bar title
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        // Enabling Spinner dropdown navigation
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        // Spinner title navigation data
+        navSpinner = new ArrayList<SpinnerNavItem>();
+        navSpinner.add(new SpinnerNavItem("SimplyGuest Intro", R.drawable.ic_slideshow_black_24dp));
+        navSpinner.add(new SpinnerNavItem("Features of our Flats", R.drawable.ic_home_black_24dp));
+        navSpinner.add(new SpinnerNavItem("House Rules", R.drawable.ic_view_list_black_24dp));
+        navSpinner.add(new SpinnerNavItem("Short Listed", R.drawable.ic_favorite_border_black_24dp));
+
+        // title drop down adapter
+        menuAdapter = new MenuNavigationAdapter(getApplicationContext(), navSpinner);
+
+        // assigning the spinner navigation
+        actionBar.setListNavigationCallbacks(menuAdapter, (ActionBar.OnNavigationListener) this);
+    }
+
+    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+        // Action to be taken after selecting a spinner item
+        return false;
+    }
+
+    public void onClickLogin(View v) {
 
         Intent intent = new Intent(this, LoginPage.class);
         startActivity(intent);
 
     }
+
 }
--- app/src/main/java/com/example/roopa/simplyguest/LoginPage.java
+++ app/src/main/java/com/example/roopa/simplyguest/LoginPage.java
@@ -1,13 +1,22 @@
 package com.example.roopa.simplyguest;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginPage extends AppCompatActivity {
+
+    Button login;
+    EditText et_email, et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +25,34 @@ public class LoginPage extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        login = (Button) findViewById(R.id.loginButton);
+        et_email = (EditText) findViewById(R.id.loginEmail);
+        et_password = (EditText) findViewById(R.id.loginPassword);
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = et_email.getText().toString();
+                String password = et_password.getText().toString();
+                if (email.equals("") || email.equals("Email")||password.equals("")||password.equals("Password")) {
+                    Toast.makeText(getApplicationContext(), "Please enter all the details!!", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                        Toast.makeText(getApplication(),"Invalid email address",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+        });
     }
 
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menus, menu);
+        return true;
+    }
 }
+
+
+
